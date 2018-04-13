73311e33545525cef43f6f4efa8d6a8c2677e481
bill.bennett <bill.bennett@tubemogul.com>
2018-04-13 13:15:14 -0700

    adding fiddle route

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import logging
 import url_manager
-from flask import Flask, redirect, request, render_template
+from flask import Flask, redirect, request, render_template, make_response
 
 logging.basicConfig(format='[%(asctime)s]:[%(levelname)s]:[%(filename)s %(lineno)d]:[%(funcName)s]:%(message)s')
 logger = logging.getLogger('run')
@@ -39,10 +39,25 @@ def empty_lookup():
 @app.route('/s/<string:short_url_hash>/')
 def url_lookup(short_url_hash):
     redirect_url = url_manager.return_url_from_short_hash(short_url_hash)
+    logger.info('Sending back: {0}'.format(redirect_url))
     if not redirect_url:
         return 'INVALID LOOKUP'
     else:
         return redirect(redirect_url, code=302)
+
+
+@app.route('/foo/', methods=['GET', 'POST'])
+def foo():
+    headers, cookies = request.headers, request.cookies
+    # logger.info('Headers: {0}'.format(headers))
+    logger.info('Cookies: {0}'.format(cookies))
+    resp = make_response('HELLO WORLD!')
+    # resp = make_response(render_template('new_url_return.html', short_url_hash='foobar'))
+    resp.set_cookie('urluid', value='testing01')
+    resp.set_cookie('sessId', value='abc')
+    resp.headers['X-HELLO'] = 'monkey'
+
+    return resp
 
 
 if __name__ == '__main__':
